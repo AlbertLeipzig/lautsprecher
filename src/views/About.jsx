ea69c609e1be24289e1aecb7dec5d6c5cfcd288b
AlbertLeipzig <albertclemvill@gmail.com>
2022-11-14 14:14:49 +0100

    impressum content

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -2,6 +2,10 @@ export const About = () => {
   return (
     <div className="about">
       <h1>About</h1>
+      <h2>Mission</h2>
+      <p>
+        Verbesserung des Leipziger Musiklebens durch Vernetzung von Menschen
+      </p>
     </div>
   );
 };
