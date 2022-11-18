0586031f897b88be48e3a792dfd31c6080669639
AlbertLeipzig <albertclemvill@gmail.com>
2022-11-18 11:01:42 +0100

    added calendar

--- src/views/Events.jsx
+++ src/views/Events.jsx
@@ -1,4 +1,5 @@
 import eventsData from '../data/events.json';
+import { Calendar } from '../components/Calendar';
 
 const events = eventsData.events;
 
@@ -12,37 +13,40 @@ const formatStartDate = (eventStart) => {
 
 export const Events = () => {
   return (
-    <div className="calendar">
+    <>
       <h1>Veranstaltungen</h1>
-      <div className="events-container">
-        {events &&
-          events.map((event, id) => (
-            <div className="event" key={id}>
-              <h2>{event.name}</h2>
-              <p>{event.description}</p>
-              <p>{event.venueId}</p>
-              {event.bandId &&
-                event.bandId.map((band, id) => <p key={id}>{band.id}</p>)}
-              {/* {event.musicians &&
+      <Calendar />
+      <div className="calendar">
+        <div className="events-container">
+          {events &&
+            events.map((event, id) => (
+              <div className="event" key={id}>
+                <h2>{event.name}</h2>
+                <p>{event.description}</p>
+                <p>{event.venueId}</p>
+                {event.bandId &&
+                  event.bandId.map((band, id) => <p key={id}>{band.id}</p>)}
+                {/* {event.musicians &&
                 event.musicians.map((musician, id) => (
                   <p key={musician.id}>{musician.id}</p>
                 ))} */}
-              {event.tags &&
-                event.tags.map((tag, id) => <p key={id}>{tag.id}</p>)}
-              <p>{event.organizerId}</p>
-              {/* {event.eventLinks && event.eventLinks.map((link, id) => (
+                {event.tags &&
+                  event.tags.map((tag, id) => <p key={id}>{tag.id}</p>)}
+                <p>{event.organizerId}</p>
+                {/* {event.eventLinks && event.eventLinks.map((link, id) => (
                 console.log(link)
               ))} */}
-              {/* {console.log(event.eventLinks)} */}
-              {/* {event.eventLinks &&
+                {/* {console.log(event.eventLinks)} */}
+                {/* {event.eventLinks &&
                 event.eventLinks.map((eventLink, id) => (
                   <p key={id}>{eventLink.id}</p>
                 ))} */}
-              <p>{event.eventLinks}</p>
-              {/*  <p>{formatStartDate(event.start)}</p> */}
-            </div>
-          ))}
+                <p>{event.eventLinks}</p>
+                {/*  <p>{formatStartDate(event.start)}</p> */}
+              </div>
+            ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
